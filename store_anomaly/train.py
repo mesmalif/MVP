c2b564923949b8f568149762fc1a6baed721d950
Mohammad Esmalifalak <34637500+mesmalif@users.noreply.github.com>
2022-07-16 12:31:07 -0400

    data path revised

--- store_anomaly/train.py
+++ store_anomaly/train.py
@@ -1,7 +1,7 @@
 from helper import read_clean_data, train_save_model, plot_train
 
 if __name__=="__main__":
-    file_path = './data/ecomerce.csv'
+    file_path = '../data/ecomerce.csv'
     df_cleaned = read_clean_data(file_path)
     trained_model = train_save_model(df_cleaned)
     df_cleaned.loc[:,'anomalies'] = trained_model.predict(df_cleaned.values)
