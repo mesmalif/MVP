bf5baafdd9efbe70bcdb732aa77ec4c5ba36dd36
neginprn <piran.negin.n@gmail.com>
2022-08-01 20:34:15 -0400

    folder revised

--- anomaly_detection/store_anomaly/predict.py
+++ anomaly_detection/store_anomaly/predict.py
@@ -3,7 +3,7 @@ from helper import plot_train as plot_predict
 from pickle import load
 
 if __name__=="__main__":
-    file_path = '../data/prediction_data.csv'
+    file_path = '../../data/prediction_data.csv'
     df_cleaned = read_clean_data(file_path)
     trained_model = load(open('anomaly_detector.pkl', 'rb'))
     df_cleaned.loc[:,'anomalies'] = trained_model.predict(df_cleaned.values)
